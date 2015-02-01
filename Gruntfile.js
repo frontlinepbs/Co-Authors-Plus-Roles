001d5b7883a1007b47f255bd8159e4fdc9f836a0
Nathaniel Taintor <goldenapplesdesign@gmail.com>
2015-02-01 02:11:29 -0800

    Add phpunit to watch task.
    Trying to speed up development a little bit with quicker feedback on
    tests.

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,11 +15,20 @@ module.exports = function(grunt) {
         bin: 'vendor/bin/phpunit',
         bootstrap: 'tests/bootstrap.php',
       },
+    },
+
+    watch: {
+      php: {
+        files: ['*.php','includes/**/*.php','tests/**/*.php'],
+        tasks: ['phpunit']
+      }
     }
+
   });
 
   grunt.loadNpmTasks('grunt-wp-readme-to-markdown');
   grunt.loadNpmTasks('grunt-phpunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask( 'wp', ['wp_readme_to_markdown']);
   grunt.registerTask( 'test', ['phpunit']);
