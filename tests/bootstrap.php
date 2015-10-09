c3976b686e86bfff79f5acd531a8d92752c01da1
Nathaniel Taintor <goldenapplesdesign@gmail.com>
2015-10-09 10:53:40 -0400

    Um, yeah, capitalize repository name for CAP

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,7 +10,7 @@ require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
 	global $_tests_dir;
-	require $_tests_dir . '/wp-content/plugins/co-authors-plus/co-authors-plus.php';
+	require $_tests_dir . '/wp-content/plugins/Co-Authors-Plus/co-authors-plus.php';
 	include dirname( __FILE__ ) . '/../co-authors-plus-roles.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
